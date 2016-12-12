09ded4fa73dabc98ac1b63683a6dd66191922e89
Martin Olsson <martin@olsson.com>
2016-12-12 11:49:51 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
     console.log( "document loaded" );
-    $('._toggleSpan').click(function(){
+    $('._toggleSpan').click(function() {
+        $('._toggleSpan').removeClass('spanCols');
         $(this).toggleClass('spanCols');
     });
 });
